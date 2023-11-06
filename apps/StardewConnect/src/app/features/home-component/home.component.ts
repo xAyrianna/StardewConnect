a63286a17a01b8eba652c0291ba2455335a8bb06
xAyrianna <aechatlein@gmail.com>
2023-11-06 16:00:12 +0100

    Added home and about component

---
+++ apps/StardewConnect/src/app/features/home-component/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stardew-connect-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {}
