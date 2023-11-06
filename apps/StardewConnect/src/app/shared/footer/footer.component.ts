e9ca3575826118c1c6d8f3e6f96f27fabaea863e
xAyrianna <aechatlein@gmail.com>
2023-11-06 16:33:27 +0100

    got rid of unused methods

--- apps/StardewConnect/src/app/shared/footer/footer.component.ts
+++ apps/StardewConnect/src/app/shared/footer/footer.component.ts
@@ -1,17 +1,9 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'stardew-connect-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit{
-  constructor() {}
-  ngOnInit(): void {
-   
-  }
-
-  
-}
+export class FooterComponent{}
