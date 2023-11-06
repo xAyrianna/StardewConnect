7117c9c6761f837d11aa474a3eb50e029a011d10
xAyrianna <aechatlein@gmail.com>
2023-11-06 16:38:40 +0100

    Added routing for town and event

--- apps/StardewConnect/src/app/app-routing.module.ts
+++ apps/StardewConnect/src/app/app-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home-component/home.component';
+import { AboutComponent } from './features/about-component/about.component';
+import { TownListComponent } from './features/town-component/town-list/town-list.component';
+import { TownEditComponent } from './features/town-component/town-edit/town-edit.component';
+import { TownDetailComponent } from './features/town-component/town-detail/town-detail.component';
+import { EventListComponent } from './features/event-component/event-list/event-list.component';
+import { EventEditComponent } from './features/event-component/event-edit/event-edit.component';
+import { EventDetailComponent } from './features/event-component/event-detail/event-detail.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
+
+  { path: 'town', component: TownListComponent, pathMatch: 'full' },
+  { path: 'town/new', component: TownEditComponent, pathMatch: 'full' },
+  { path: 'town/:id', component: TownDetailComponent, pathMatch: 'full' },
+  { path: 'town/:id/edit', component: TownEditComponent, pathMatch: 'full' },
+
+  { path: 'event', component: EventListComponent, pathMatch: 'full' },
+  { path: 'event/new', component: EventEditComponent, pathMatch: 'full' },
+  { path: 'event/:id', component: EventDetailComponent, pathMatch: 'full' },
+  { path: 'event/:id/edit', component: EventEditComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-  })
-  export class AppRoutingModule {}
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
