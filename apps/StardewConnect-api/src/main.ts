022802f90a45a63845e2b3678f467ee5accea710
xAyrianna <aechatlein@gmail.com>
2023-11-07 16:48:51 +0100

    Add Swagger setup to API

--- apps/StardewConnect-api/src/main.ts
+++ apps/StardewConnect-api/src/main.ts
@@ -1,12 +1,7 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { setupSwagger } from '../swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env['PORT'] || 3333;
   app.enableCors();
+  setupSwagger(app);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
