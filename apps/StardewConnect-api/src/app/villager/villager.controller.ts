3e5efe94e7af74ef260b52c31d590634e0159a0b
xAyrianna <aechatlein@gmail.com>
2023-11-07 15:25:23 +0100

    Implemented functionality for villager route

--- apps/StardewConnect-api/src/app/villager/villager.controller.ts
+++ apps/StardewConnect-api/src/app/villager/villager.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Villager } from '@StardewConnect/libs/data';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { VillagerService } from './villager.service';
 
 @Controller('villager')
-export class VillagerController {}
+export class VillagerController {
+    constructor(private readonly villagerService: VillagerService) {}
+  
+    @Get()
+    getAll() {
+      return this.villagerService.getAll();
+    }
+  
+    @Get(':id')
+    getVillagerByName(@Param('id') name: string) {
+      return this.villagerService.getVillagerByName(name);
+    }
+    @Post()
+    addVillager(@Body() user: Villager) {
+      this.villagerService.addVillager(user);
+    }
+  
+    @Put()
+    updateVillager(@Body() updatedUser: Villager) {
+      this.villagerService.updateVillager(updatedUser);
+    }
+  
+    @Delete()
+    deleteVillager(@Body() deletedUser: Villager) {
+      this.villagerService.deleteVillager(deletedUser);
+    }
+  }
--- apps/StardewConnect-api/src/app/villager/villager.service.ts
+++ apps/StardewConnect-api/src/app/villager/villager.service.ts
@@ -1,4 +1,94 @@
+import { Gender, LifeStage, Villager } from '@StardewConnect/libs/data';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class VillagerService {}
+export class VillagerService {
+    villagers: Villager[] = [
+      {
+        id: 1,
+        name: 'Evelyn',
+        gender: Gender.Female,
+        lifeStage: LifeStage.Elder,
+        marriageable: false,
+        birthday: 'Winter 20',
+        favoriteGifts: [
+          'Beet',
+          'Chocolate Cake',
+          'Diamond',
+          'Fairy Rose',
+          'Stuffing',
+          'Tulip',
+        ],
+      },
+      {
+        id: 2,
+        name: 'Sophia',
+        gender: Gender.Female,
+        lifeStage: LifeStage.Adult,
+        marriageable: true,
+        birthday: 'Winter 27',
+        favoriteGifts: [
+          'Fairy Stone',
+          'Grampleton Orange Chicken',
+          'Fairy Rose',
+          'Puppyfish',
+        ],
+      },
+      {
+        id: 3,
+        name: 'Morgan',
+        gender: Gender.Other,
+        lifeStage: LifeStage.Child,
+        marriageable: false,
+        birthday: 'Fall 7',
+        favoriteGifts: ['Iridium Bar', 'Void Egg', 'Void Mayonnaise'],
+      },
+      {
+        id: 4,
+        name: 'Victor',
+        gender: Gender.Male,
+        lifeStage: LifeStage.Adult,
+        marriageable: true,
+        birthday: 'Summer 23',
+        favoriteGifts: [
+          'Aged Blue Moon Wine',
+          'Spaghetti',
+          'Battery Pack',
+          'Duck Feather',
+          'Lunarite',
+        ],
+      },
+    ];
+  
+    private getIndexById(villager: Villager): number {
+      return this.villagers.findIndex((v) => v.id === villager.id);
+    }
+  
+    getAll() {
+      return { results: this.villagers };
+    }
+  
+    getVillagerByName(name: string) {
+      return {
+        results: this.villagers.filter((villager) => villager.name === name)[0],
+      };
+    }
+  
+    addVillager(newVillager: Villager) {
+      newVillager.id = this.villagers.at(this.villagers.length - 1)!.id + 1;
+      console.log(newVillager.id);
+      this.villagers.push(newVillager);
+    }
+  
+    updateVillager(updatedVillager: Villager) {
+      const index = this.getIndexById(updatedVillager);
+      this.villagers[index] = updatedVillager;
+    }
+  
+    deleteVillager(deletedVillager: Villager) {
+      // delete villager
+      const index = this.getIndexById(deletedVillager);
+      this.villagers.splice(index, 1);
+      console.log(this.villagers);
+    }
+  }
