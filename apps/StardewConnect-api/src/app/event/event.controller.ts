3926448a5db7065e0c5a8fdaad963ec5f2c5503a
xAyrianna <aechatlein@gmail.com>
2023-11-07 15:17:17 +0100

    Implemented functionality for event route

--- apps/StardewConnect-api/src/app/event/event.controller.ts
+++ apps/StardewConnect-api/src/app/event/event.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { EventService } from './event.service';
+import { Event } from '@StardewConnect/libs/data';
 
 @Controller('event')
-export class EventController {}
+export class EventController {
+    constructor(private readonly eventService: EventService){}
+
+    @Get()
+    getAll(){
+      return this.eventService.getAll();
+    }
+  
+    @Get(':id')
+    getEventByUsername(@Param('id') name: string) {
+      return this.eventService.getEventByName(name);
+    }
+    @Post()
+    addEvent(@Body() event: Event) {
+      this.eventService.addEvent(event);
+    }
+  
+    @Put()
+    updateEvent(@Body() updatedEvent: Event) {
+      this.eventService.updateEvent(updatedEvent);
+    }
+  
+    @Delete()
+    deleteEvent(@Body() deletedEvent: Event) {
+      this.eventService.deleteEvent(deletedEvent);
+    }
+}
+
--- apps/StardewConnect-api/src/app/event/event.service.ts
+++ apps/StardewConnect-api/src/app/event/event.service.ts
@@ -1,4 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import { Event } from '@StardewConnect/libs/data';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+    events: Event[] = [
+      {
+        id: 1,
+        name: 'Luau',
+        description:
+          'A pot luck event where villagers bring something to contribute to the pan!',
+        date: 'Summer 11',
+        location: 'Beach',
+        hasHappened: false,
+      },
+      {
+        id: 2,
+        name: "Spirit's Eve",
+        description:
+          "An evening full of spooky events. There's a labyrinth, finish it and maybe there's a price...",
+        date: 'Fall 27',
+        location: "Town's square",
+        hasHappened: true,
+      },
+      {
+        id: 3,
+        name: 'Feast of the Winter Star',
+        description:
+          'A feast where everybody from town enjoys dinner together and participate to the secret gift-giving',
+        date: 'Winter 25',
+        location: "Town's square",
+        hasHappened: false,
+      },
+      {
+        id: 4,
+        name: 'Flower dance',
+        description:
+          "Every year there's a flower dance. It's tradition. It's possible to dance with the bachelors and bachelorettes.",
+        date: 'Spring 24',
+        location: 'Forest',
+        hasHappened: false,
+      },
+    ];
+    private getIndexById(event: Event): number {
+      return this.events.findIndex((e) => e.id === event.id);
+    }
+  
+    getAll() {
+      return { results: this.events };
+    }
+  
+    getEventByName(name: string) {
+      return { results: this.events.filter((event) => event.name === name)[0] };
+    }
+  
+    addEvent(newEvent: Event) {
+      newEvent.id = this.events.at(this.events.length - 1)!.id + 1;
+      console.log(newEvent.id);
+      this.events.push(newEvent);
+    }
+  
+    updateEvent(updatedEvent: Event) {
+      const index = this.getIndexById(updatedEvent);
+      this.events[index] = updatedEvent;
+    }
+  
+    deleteEvent(deletedTown: Event) {
+      // delete event
+      const index = this.getIndexById(deletedTown);
+      this.events.splice(index, 1);
+      console.log(this.events);
+    }
+  }
