9df0453cb539008216b28bbfdbbb013c20c3aad4
xAyrianna <aechatlein@gmail.com>
2023-11-07 15:14:50 +0100

    generated controllers and services, auth, event, town, user, villager

--- apps/StardewConnect-api/src/app/app.module.ts
+++ apps/StardewConnect-api/src/app/app.module.ts
@@ -2,10 +2,34 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TownController } from './town/town.controller';
+import { TownService } from './town/town.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { EventController } from './event/event.controller';
+import { EventService } from './event/event.service';
+import { VillagerService } from './villager/villager.service';
+import { VillagerController } from './villager/villager.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    TownController,
+    UserController,
+    EventController,
+    VillagerController,
+    AuthController,
+  ],
+  providers: [
+    AppService,
+    TownService,
+    UserService,
+    EventService,
+    VillagerService,
+    AuthService,
+  ],
 })
 export class AppModule {}
---
+++ apps/StardewConnect-api/src/app/villager/villager.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('villager')
+export class VillagerController {}
