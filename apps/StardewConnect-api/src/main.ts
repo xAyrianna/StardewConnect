638b7dc39c1ad6dbf35f8b81beaf79ca7815aac4
xAyrianna <aechatlein@gmail.com>
2023-11-14 12:55:56 +0100

    Add JWT_SECRET to .env file and enable CORS with options

--- apps/StardewConnect-api/src/main.ts
+++ apps/StardewConnect-api/src/main.ts
@@ -2,13 +2,16 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { setupSwagger } from '../swagger/swagger.config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env['PORT'] || 3333;
-  app.enableCors();
+  
+  const corsOPtions : CorsOptions = {};
+  app.enableCors(corsOPtions);
   setupSwagger(app);
   await app.listen(port);
   Logger.log(
