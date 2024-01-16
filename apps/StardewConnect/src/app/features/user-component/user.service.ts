15a4f270d567001395ba0e5adcb82c3edac8be6a
xAyrianna <aechatlein@gmail.com>
2024-01-16 16:22:31 +0100

    Remove bcrypt import and async keyword from addUser and updateUser methods

--- apps/StardewConnect/src/app/features/user-component/user.service.ts
+++ apps/StardewConnect/src/app/features/user-component/user.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable, map, tap } from 'rxjs';
 import { ApiResponse, User } from '@StardewConnect/libs/data';
-// import * as bcrypt from 'bcrypt'; 
 
 @Injectable({
   providedIn: 'root'
@@ -33,16 +32,14 @@ export class UserService {
       .pipe(map((response: ApiResponse<User>) => response.results));
   }
 
-  async addUser(newUser: User) {
+   addUser(newUser: User) {
     console.log("Adding new user")
     const userUrl = this.BASE_URL + '/user';
     console.log('post ' + userUrl);
-    // newUser.password = await bcrypt.hash(newUser.password, 10);
     return this.http.post<User>(userUrl, newUser);
   }
-  async updateUser(updatedUser: User){
+   updateUser(updatedUser: User){
     const userUrl = this.BASE_URL + '/user';
-    // updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
     return this.http.put<User>(userUrl, updatedUser);
   }
 
