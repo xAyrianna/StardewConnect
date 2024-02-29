80b2d99e1a38a1bc71f8861fae7dbac46a16acd9
xAyrianna <aechatlein@gmail.com>
2024-02-29 14:56:07 +0100

    created login and register components

--- apps/StardewConnect/src/app/app.module.ts
+++ apps/StardewConnect/src/app/app.module.ts
@@ -21,6 +21,10 @@ import { UserEditComponent } from './features/user-component/user-edit/user-edit
 import { UserDetailComponent } from './features/user-component/user-detail/user-detail.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponentComponent } from './features/login-component.component';
+import { LoginComponentComponent } from './features/login-component/login-component.component';
+import { LoginComponent } from './features/login-component/login.component';
+import { RegisterComponent } from './features/register-component/register.component';
 
 @NgModule({
   declarations: [
@@ -43,6 +47,10 @@ import { HttpClientModule } from '@angular/common/http';
     UserListComponent,
     UserEditComponent,
     UserDetailComponent,
+    LoginComponentComponent,
+    LoginComponentComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ apps/StardewConnect/src/app/features/register-component/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stardew-connect-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {}
