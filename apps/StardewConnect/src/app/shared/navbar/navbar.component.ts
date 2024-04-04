3367155c4ee6542c02e27f5d88111836bf5b5362
Ayrianna Chatlein <aechatlein@gmail.com>
2024-04-04 14:57:32 +0200

    Update footer and navbar components

--- apps/StardewConnect/src/app/shared/navbar/navbar.component.ts
+++ apps/StardewConnect/src/app/shared/navbar/navbar.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'stardew-connect-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  // set anchor element active based on current route
+  activeRoute = window.location.pathname;
+
+  ngOnInit(): void {
+    console.log('Current route: ' + this.activeRoute);
+  }
+}
