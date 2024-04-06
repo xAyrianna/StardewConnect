25c990cc62a92b0f05656abadc1c7fa6589abc34
Ayrianna Chatlein <aechatlein@gmail.com>
2024-04-06 14:31:09 +0200

    Update navbar component to handle user authentication

--- apps/StardewConnect/src/app/shared/navbar/navbar.component.ts
+++ apps/StardewConnect/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'stardew-connect-navbar',
@@ -8,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   // set anchor element active based on current route
   activeRoute = window.location.pathname;
+  loggedIn = false;
+  
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     console.log('Current route: ' + this.activeRoute);
+    if (localStorage.getItem('access_token')) {
+      this.loggedIn = true;
+    }
+  }
+
+  logout(): void {
+    console.log('Logging out');
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('user_ID');
+    this.loggedIn = false;
+    this.router.navigate(['/login']);
   }
 }
