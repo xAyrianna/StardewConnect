606bd3d154f1381f35f3328c6abda52c1a270a61
Ayrianna Chatlein <aechatlein@gmail.com>
2024-04-06 14:29:27 +0200

    Add User module and service to EventModule and TownModule

--- apps/StardewConnect-api/src/app/event/event.module.ts
+++ apps/StardewConnect-api/src/app/event/event.module.ts
@@ -5,16 +5,19 @@ import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { Town, TownSchema } from '../town/schemas/town.schema';
 import { TownService } from '../town/town.service';
+import { User, UserSchema } from '../user/schemas/user.schema';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Event.name, schema: EventSchema },
       { name: Town.name, schema: TownSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [EventController],
-  providers: [EventService, TownService],
+  providers: [EventService, TownService, UserService],
   exports: [EventService],
 })
 export class EventModule {}
--- apps/StardewConnect-api/src/app/town/town.module.ts
+++ apps/StardewConnect-api/src/app/town/town.module.ts
@@ -5,16 +5,19 @@ import { TownController } from './town.controller';
 import { TownService } from './town.service';
 import { EventService } from '../event/event.service';
 import { Event, EventSchema } from '../event/schemas/event.schema';
+import { User, UserSchema } from '../user/schemas/user.schema';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Town.name, schema: TownSchema },
       { name: Event.name, schema: EventSchema },
+      { name: User.name, schema: UserSchema}
     ]),
   ],
   controllers: [TownController],
-  providers: [TownService, EventService],
+  providers: [TownService, EventService, UserService],
   exports: [TownService],
 })
 export class TownModule {}
