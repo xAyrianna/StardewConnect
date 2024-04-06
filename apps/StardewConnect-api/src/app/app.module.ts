06d8105b0ec8689628a2f56dd52ca433172bd071
Ayrianna Chatlein <aechatlein@gmail.com>
2024-04-06 14:29:08 +0200

    Add Neo4j database integration and configuration

--- apps/StardewConnect-api/src/app/app.module.ts
+++ apps/StardewConnect-api/src/app/app.module.ts
@@ -17,10 +17,19 @@ import { TownModule } from './town/town.module';
 import { UserModule } from './user/user.module';
 import { VillagerModule } from './villager/villager.module';
 import { AuthModule } from './auth/auth.module';
+import { Neo4jScheme } from './neo4j/neo4j.config.interface';
+import { Neo4jModule } from './neo4j/neo4j.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_CONN),
+    Neo4jModule.forRootAsync({
+      scheme: process.env.NEO4J_SCHEME as Neo4jScheme,
+      host: process.env.NEO4J_HOST,
+      username: process.env.NEO4J_USR,
+      password: process.env.NEO4J_PWD,
+      database: process.env.NEO4J_DATABASE,
+  }),
     EventModule,
     TownModule,
     UserModule,
