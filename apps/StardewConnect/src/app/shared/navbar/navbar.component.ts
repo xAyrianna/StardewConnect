1890b2de812edeee47c5655e3fc29c8075769dd7
Ayrianna Chatlein <aechatlein@gmail.com>
2024-04-09 00:01:21 +0200

    Refactor navbar component to include user profile link

--- apps/StardewConnect/src/app/shared/navbar/navbar.component.ts
+++ apps/StardewConnect/src/app/shared/navbar/navbar.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../features/user-component/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'stardew-connect-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   // set anchor element active based on current route
   activeRoute = window.location.pathname;
   loggedIn = false;
-  
-  constructor(private router: Router, userService: UserService) {
+  loggedInUser: string | undefined;
+  subscription: Subscription | undefined;
+
+  constructor(private router: Router, private userService: UserService) {
     userService.userLoggedIn.subscribe((value) => {
       this.loggedIn = value;
     });
@@ -22,6 +25,21 @@ export class NavbarComponent implements OnInit {
     console.log('Current route: ' + this.activeRoute);
     if (localStorage.getItem('access_token')) {
       this.loggedIn = true;
+      const userId = localStorage.getItem('user_ID');
+      if (userId) {
+        this.subscription = this.userService
+          .getUserByID(userId)
+          .subscribe((response) => {
+            this.loggedInUser = response.username;
+          });
+      }
+    }
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroying event-list component');
+    if (this.subscription !== undefined) {
+      this.subscription.unsubscribe();
     }
   }
 
